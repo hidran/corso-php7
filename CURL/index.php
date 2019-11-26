57dd629b106d74e011f36ffbf3885e650b1b2411
Hidran <hidran@gmail.com>
2019-11-26 01:12:16 +0100

    adds css and bootstrap template

--- CURL/index.php
+++ CURL/index.php
@@ -1,34 +1,147 @@
 <html>
     <head>
         <title>my posts</title>
-        <style>
-            ul {
-                list-style-type: none;
-            }
-            li {
-                padding:4px;
-                border-bottom: 1px solid #c0c0c0;
-            }
-        </style>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+        <link href="css/clean-blog.min.css" rel="stylesheet">
     </head>
     <body>
-        <?php
-        $url = 'https://jsonplaceholder.typicode.com/posts';
+        <!-- Navigation -->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="about.html">About</a>
+                        </li>
 
-        $data = file_get_contents($url);
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.html">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Page Header -->
+        <header class="masthead" style="background-image: url('img/home-bg.jpg')">
+            <div class="overlay"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-md-10 mx-auto">
+                        <div class="site-heading">
+                            <h1>Clean Blog</h1>
+                            <span class="subheading">A Blog Theme by Start Bootstrap</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- Main Content -->
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-md-10 mx-auto">
+                    <?php
+                    $url = 'https://jsonplaceholder.typicode.com/post2s';
 
-        if ($data) {
-            $dataObj = json_decode($data);
-            ?>
-            <ul>
-                <?php
-                foreach ($dataObj as $post) {
-                   echo "<li><a href='$url/$post->id'>$post->title.</a></li>" ;
-                }
-                ?>
-            </ul>
-                <?php
-            }
-            ?>
+                    $context = stream_context_create([
+                        'http' => [
+                            'ignore_errors' => 1,
+                            'timeout' => 5 ,
+                            
+                        ]
+                    ]);
+
+                    $data = file_get_contents($url, false, $context);
+                    $datepost = date('Y-m-d H:i:s');
+                    $postby = 'Hidran Arias';
+                    if ($data) {
+
+                        $dataObj = json_decode($data);
+                        if ($dataObj && is_array($dataObj)) {
+
+                            foreach ($dataObj as $post) {
+                                ?>
+                                <div class="post-preview">
+                                    <a href="<?= $url . '/' . $post->id ?>">
+                                        <h2 class="post-title">
+                                            <?= $post->title ?>
+                                        </h2>
+                                    </a>
+                                    <div class="post-body">
+                                        <?= $post->body ?>
+                                    </div>
+
+                                    <p class="post-meta">Posted by
+                                        <a href="<?= $userUrl . '/' . $post->userId ?>"><?= $postby ?></a>
+                                        <?= $datepost ?></p>
+                                </div>
+
+                                <?php
+                            }
+                            ?>
+                            </ul>
+                            <?php
+                        } else {
+                            echo '<h2>NO POSTS</h2>';
+                        }
+                    }
+                    ?>
+
+
+                    <!-- Pager -->
+                    <div class="clearfix">
+                        <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-md-10 mx-auto">
+                        <ul class="list-inline text-center">
+                            <li class="list-inline-item">
+                                <a href="#">
+                                    <span class="fa-stack fa-lg">
+                                        <i class="fas fa-circle fa-stack-2x"></i>
+                                        <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
+                                    </span>
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#">
+                                    <span class="fa-stack fa-lg">
+                                        <i class="fas fa-circle fa-stack-2x"></i>
+                                        <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
+                                    </span>
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#">
+                                    <span class="fa-stack fa-lg">
+                                        <i class="fas fa-circle fa-stack-2x"></i>
+                                        <i class="fab fa-github fa-stack-1x fa-inverse"></i>
+                                    </span>
+                                </a>
+                            </li>
+                        </ul>
+                        <p class="copyright text-muted">Copyright &copy; Your Website 2019</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
     </body>
 </html>
